fe37a72c09ced33863d796b2cf64bb0ceae248b8
prashantdawar <prashantdawar.2008@gmail.com>
2019-02-10 17:57:15 +0530

    init

--- app.js
+++ app.js
@@ -8,48 +8,48 @@ let port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 3001;
 //
 //
 //
-// if (cluster.isMaster) {
-//   console.log(`Master ${process.pid} is running`);
+if (cluster.isMaster) {
+  console.log(`Master ${process.pid} is running`);
 
-//   // Fork workers.
-//   for (let i = 0; i < numCPUs; i++) {
-//     cluster.fork();
-//   }
+  // Fork workers.
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
 
-//   cluster.on("exit", (worker, code, signal) => {
-//     console.log(`worker ${worker.process.pid} died`);
-//   });
-// } else {
-// Workers can share any TCP connection
-server
-  .on("request", (req, res) => {
-    res.setHeader("content-type", "application/json");
-    let host = req.headers.host;
-    if (host.length > 50) {
-      res.statusCode = 404;
-      res.end();
-      return;
-    }
-    let subDomain = host.slice(0, host.indexOf(":")).split(".");
-    let domain = subDomain.pop();
-    if (!isNaN(domain)) {
-      res.statusCode = 502;
-      res.end();
-      //
-      return;
-    }
-    // res.write(JSON.stringify(Object.keys(req)));
-    res.write(JSON.stringify(host));
-    res.end();
-    return;
-  })
-  .listen(port, hostname, () => {
-    console.log(`Server is running at PORT: ${port}`);
-    // setTimeout(() => {
-    //   process.exit();
-    // }, 7000 - Math.random() * 5000);
+  cluster.on("exit", (worker, code, signal) => {
+    console.log(`worker ${worker.process.pid} died`);
   });
+} else {
+  // Workers can share any TCP connection
+  server
+    .on("request", (req, res) => {
+      res.setHeader("content-type", "application/json");
+      let host = req.headers.host;
+      if (host.length > 50) {
+        res.statusCode = 404;
+        res.end();
+        return;
+      }
+      let subDomain = host.slice(0, host.indexOf(":")).split(".");
+      let domain = subDomain.pop();
+      if (!isNaN(domain)) {
+        res.statusCode = 502;
+        res.end();
+        //
+        return;
+      }
+      // res.write(JSON.stringify(Object.keys(req)));
+      res.write(JSON.stringify(host));
+      res.end();
+      return;
+    })
+    .listen(port, hostname, () => {
+      console.log(`Server is running at PORT: ${port}`);
+      // setTimeout(() => {
+      //   process.exit();
+      // }, 7000 - Math.random() * 5000);
+    });
 
-// console.log(`Worker ${process.pid} started`);
-// }
+  // console.log(`Worker ${process.pid} started`);
+}
 module.exports = server;
