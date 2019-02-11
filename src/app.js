fb514f21d07e4ffcf2dac3af36873ac016749822
prashantdawar <prashantdawar.2008@gmail.com>
2019-02-11 07:35:07 +0530

    update

--- src/app.js
+++ src/app.js
@@ -1,21 +1,21 @@
 //
 const cluster = require("cluster");
-const numCPUs = require("os").cpus().length;
-const workerProcess = numCPUs > 4 ? numCPUs : 4;
+const os = require("os");
+const numCPUs = os.cpus().length;
 let server = require("http").createServer();
 const url = require("url");
+const path = require("path");
+const fs = require("fs");
+//
+//
+const workerProcess = numCPUs > 4 ? numCPUs : 4;
 let port = process.env.PORT || 3001;
-//
-//
-//
 if (cluster.isMaster) {
   console.log(`Master ${process.pid} is running`);
-
   // Fork workers.
   for (let i = 0; i < workerProcess; i++) {
     cluster.fork();
   }
-
   cluster.on("exit", (worker, code, signal) => {
     console.log(`worker ${worker.process.pid} died`);
   });
@@ -25,20 +25,26 @@ if (cluster.isMaster) {
     .on("request", (req, res) => {
       res.setHeader("content-type", "application/json");
       let promise = new Promise((resolve, reject) => {
-        // let err = {};
         if (req.method != "GET") {
           return reject({ statusCode: 405 });
         }
-        //
         // hard limit on acceptable hostname
+        //
         let host = req.headers.host;
-        if (host.length > 50) return reject({ statusCode: 404 });
-
-        let subDomain = host.slice(0, host.indexOf(":")).split(".");
+        // if (host.length > 50) return reject({ statusCode: 404 });
+        let domainURL = host.slice(0, host.indexOf(":"));
+        let subDomain = domainURL.split(".");
+        //
+        //
         let dExtenstion = subDomain.pop();
         let domain = subDomain.pop();
         // if (!isNaN(domain)) return reject({ statusCode: 502 });
-        return resolve(req.url);
+        //
+        const sanitizePath = path
+          .normalize(req.url)
+          .replace(/^(\.\.[\/\\])+/, "");
+        let pathname = path.join(os.homedir(), "www", domainURL, sanitizePath);
+        return resolve(pathname);
       });
 
       promise
